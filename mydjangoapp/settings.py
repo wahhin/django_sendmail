805b9ed6c07176cfc6145fe0b9fcfc2669d978ad
wahhin <98505610+wahhin@users.noreply.github.com>
2022-01-27 22:46:18 +0800

    Update settings.py

--- mydjangoapp/settings.py
+++ mydjangoapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,9 +129,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 ## Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'whwebapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'epbxyelktcnntxoz'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = '1025'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
+
+# embbed some js files
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
